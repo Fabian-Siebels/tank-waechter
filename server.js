b758180d160d429c92320e2c79eb8ddc3e81e21f
Fabian-Siebels <fabian.siebels@gmail.com>
2020-10-23 11:55:01 +0200

    update

--- server.js
+++ server.js
@@ -5,18 +5,59 @@
 // Temperatur mehr als 300 Min über 28°C
 // Netzwerkstörung über 300 Min
 
+// Konstanten fuer die Influx Datenbank
+const serverhost = "localhost";
+const dbname = "tankwaechter";
+const measurementname = "tempSensor";
+
+const Influx = require("influx");
+
+const influx = new Influx.InfluxDB({
+    host: serverhost,
+    database: dbname,
+    schema: [{
+        measurement: measurementname,
+        fields: {
+            temperatur: Influx.FieldType.FLOAT
+        },
+        tags: [
+            measurementname
+        ]
+    }]
+});
+
+// Fehlerindikator
+var fehlerindikator = 0;
+
+function checkDB() {
+    influx.getDatabaseNames()
+    .then(names => {
+        if (!names.includes(dbname)) {
+            fehlerindikator = 1;
+            console.log(fehlerindikator)
+        }
+    })
+}
+
 
 // Eingangsvariable
-var eingangsTemperatur = 14;
+// var eingangsTemperatur = 14;
 
 // Zeitkonstante bei 13°C
-var zeitkonstante13 = 10000;
+var zeitkonstante13 = 60000;
 
 // Zeitkonstante bei 16°C
-var zeitkonstante16 = 10000;
+var zeitkonstante16 = 36000;
 
 // Zeitkonstante bei 28°C
-var zeitkonstante28 = 10000;
+var zeitkonstante28 = 30000;
+
+// Letzte Temperatur aus der Influx holen
+function getTemp() {
+    eingangsTemperatur = 16;
+    return;
+}
+
 
 // Willi und der Zeitgeist
 var zeitSpeicher = {
@@ -31,37 +72,50 @@ var zeitSpeicher = {
 
 // UeberprüfungsAlgorithmus
 function checkTemp() {
-    if (eingangsTemperatur >= 28) {
-        console.log("Über 28C")
-        if ( zeitSpeicher.ueberT300 >= 0 && zeitSpeicher.benutzungT300 == 0) {
-            zeitSpeicher.ueberT300 = Date.now() + zeitkonstante28;
-            zeitSpeicher.benutzungT300 = 1;
-        }
-        if (Date.now() >= zeitSpeicher.ueberT300) {
-            console.log("Zeit voll über 28°C");
-            zeitSpeicher.benutzungT300 = 0;
-        }
-    } else if (eingangsTemperatur >= 16) {
-        console.log("Über 16°C")
-        if (zeitSpeicher.ueberT360 >= 0 && zeitSpeicher.benutzungT360 == 0) {
-            zeitSpeicher.ueberT360 = Date.now() + zeitkonstante16;
-            zeitSpeicher.benutzungT360 = 1;
-        }
-        if (Date.now() >= zeitSpeicher.ueberT360) {
-            console.log("Zeit Voll über 16°C!")
-            zeitSpeicher.benutzungT360 = 0;
-        }
-    } else if (eingangsTemperatur >= 13) {
-        console.log("Über 13°C")
-        if (zeitSpeicher.ueberT600 >= 0 && zeitSpeicher.benutzungT600 == 0) {
-            zeitSpeicher.ueberT600 = Date.now() + zeitkonstante13;
-            zeitSpeicher.benutzungT600 = 1;
-        } 
-        if (Date.now() >=  zeitSpeicher.ueberT600) {
-            console.log("Zeit Voll über 13°C")
-            zeitSpeicher.benutzungT600 = 0;
+    if (fehlerindikator == 1) {
+        console.log("Fehler!");
+    } else {
+        getTemp()
+        if (eingangsTemperatur >= 28) {
+            console.log("Über 28C")
+            if ( zeitSpeicher.ueberT300 >= 0 && zeitSpeicher.benutzungT300 == 0) {
+                zeitSpeicher.ueberT300 = Date.now() + zeitkonstante28;
+                zeitSpeicher.benutzungT600 = 0;
+                zeitSpeicher.benutzungT360 = 0;
+                zeitSpeicher.benutzungT300 = 1;
+            }
+            if (Date.now() >= zeitSpeicher.ueberT300) {
+                console.log("Zeit voll über 28°C");
+                zeitSpeicher.benutzungT300 = 0;
+            }
+        } else if (eingangsTemperatur >= 16) {
+            console.log("Über 16°C")
+            if (zeitSpeicher.ueberT360 >= 0 && zeitSpeicher.benutzungT360 == 0) {
+                zeitSpeicher.ueberT360 = Date.now() + zeitkonstante16;
+                zeitSpeicher.benutzungT600 = 0;
+                zeitSpeicher.benutzungT360 = 1;
+                zeitSpeicher.benutzungT300 = 0;
+            }
+            if (Date.now() >= zeitSpeicher.ueberT360) {
+                console.log("Zeit Voll über 16°C!")
+                zeitSpeicher.benutzungT360 = 0;
+            }
+        } else if (eingangsTemperatur >= 13) {
+            console.log("Über 13°C")
+            if (zeitSpeicher.ueberT600 >= 0 && zeitSpeicher.benutzungT600 == 0) {
+                zeitSpeicher.ueberT600 = Date.now() + zeitkonstante13;
+                zeitSpeicher.benutzungT600 = 1;
+                zeitSpeicher.benutzungT360 = 0;
+                zeitSpeicher.benutzungT300 = 0;
+            } 
+            if (Date.now() >=  zeitSpeicher.ueberT600) {
+                console.log("Zeit Voll über 13°C")
+                zeitSpeicher.benutzungT600 = 0;
+            }
         }
     }
 }
+
+
 
 setInterval(checkTemp, 1000);
