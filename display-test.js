9139fbaf3b7ff85271c132d3c425c09948481543
unknown <fabian.siebels@gmail.com>
2020-10-29 09:33:17 +0100

    bugfix

--- display-test.js
+++ display-test.js
@@ -1,6 +1,12 @@
 const LCD = require('raspberrypi-liquid-crystal');
 const lcd = new LCD( 1, 0x27, 16, 2 );
 
+try {
+    lcd.beginSync();
+  } catch (e) {
+    console.log(e);
+  }
+
 lcd.clearSync();
 lcd.printSync( 'Hello' );
 lcd.setCursorSync(0, 1);
